10613b72b812c51c8b39476a8e48e6fb51f0c503
roy <xf.radish@gmail.com>
2015-08-27 15:19:09 +0800

    Initial commit

---
+++ common/fis-conf.js
@@ -0,0 +1,28 @@
+/**
+ * @file FIS 配置
+ * @author
+ */
+
+fis.config.set('namespace', 'common');
+
+// chrome下可以安装插件实现livereload功能
+// https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
+fis.config.set('livereload.port', 35729);
+
+fis.media('debug').match('*', {
+  optimizer: null,
+  useHash: false,
+  deploy: fis.plugin('http-push', {
+    receiver: 'http://127.0.0.1:3000/yog/upload',
+    to: '/'
+  })
+});
+fis.media('prod').match('*', {
+  deploy: fis.plugin('http-push', {
+    receiver: 'http://127.0.0.1:3000/yog/upload',
+    to: '/'
+  })
+});
+fis.match('/client/static/js/*.js', {
+  domain: 'http://cdn.baidu.com/'
+});
---
+++ home/fis-conf.js
@@ -0,0 +1,32 @@
+/**
+ * @file FIS 配置
+ * @author
+ */
+
+fis.config.set('namespace', 'home');
+
+// chrome下可以安装插件实现livereload功能
+// https://chrome.google.com/webstore/detail/livereload/jnihajbhpnppcggbcgedagnkighmdlei
+fis.config.set('livereload.port', 35729);
+
+fis.media('debug').match('*', {
+  optimizer: null,
+  useHash: false,
+  deploy: fis.plugin('http-push', {
+    receiver: 'http://127.0.0.1:3000/yog/upload',
+    to: '/'
+  })
+});
+fis.media('prod').match('*', {
+  deploy: fis.plugin('http-push', {
+    receiver: 'http://127.0.0.1:3000/yog/upload',
+    to: '/'
+  })
+});
+
+fis.match('/client/static/js/*.js', {
+  packTo: 'p.js'
+});
+
+
+
