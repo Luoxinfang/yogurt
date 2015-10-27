143f58aba736535ed3c17d4fd046a1c7820fd931
luoxinfang <xf.radish@gmail.com>
2015-10-27 09:16:01 +0800

    session module

--- common/server/test/session.js
+++ common/server/test/session.js
@@ -4,21 +4,35 @@
  */
 
 module.exports = {
-	login: function (user, callback) {
-		var rs = {
-			status: '00000'
-		};
-		if (user.userName != '7411') {
-			rs = {
-				status: '11111',
-				exception: '用户不存在'
-			};
-		} else if (user.password != '123'){
-			rs = {
-				status: '11112',
-				exception: '密码错误'
-			};
-		}
-		callback(rs);
-	}
+  login: function (user, callback) {
+    var rs = {
+      status: '00000'
+    };
+    if (user.userName != '7411') {
+      rs = {
+        status: '11111',
+        exception: '用户不存在'
+      };
+    } else if (user.password != '123') {
+      rs = {
+        status: '11112',
+        exception: '密码错误'
+      };
+    } else {
+      rs.result = {
+        "parentId": 0,
+        "personalChannelTypeId": 0,
+        "siteLogo": "//images.hzins.com/short/www/2015/icon/logo_default.png",
+        "webType": 1,
+        "email": "331744837@qq.com",
+        "contactName": "罗新芳",
+        "domain": "lx58818",
+        "partnerId": 58818,
+        "agentId": 0,
+        "siteName": "",
+        "settleType": 0
+      }
+    }
+    callback(rs);
+  }
 };
